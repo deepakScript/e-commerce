8af65e49bb17bbb2aedd7fb10144ef9c65258e0f
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-16 20:34:51 +0545

    feat: enhance Error component with a user-friendly message for 404 pages

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
 
 const Error = () => {
   return (
-    <div>
-      Error
-    </div>
-  )
-}
+      <div className="lg:container mx-auto flex items-center justify-center">
+          <div className="min-h-[400px] w-full flex items-center justify-center">
 
-export default Error
+              <h3 className="text-4xl text-red-500 font-inter font-bold capitalize">Not Found This Page!</h3>
+          </div>
+      </div>
+  );
+};
+
+export default Error;

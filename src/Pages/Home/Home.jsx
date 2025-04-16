bf67bd00f571b256169333637841c012647bc9a7
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-16 20:38:42 +0545

    feat: add Footer component to Home page layout

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../../Components/Banner/Banner";
 import Brand from "../../Components/Brand/Brand";
 import Categories from "../../Components/Categories/Categories";
 import Client from "../../Components/Client/Client";
+import Footer from "../../Components/Footer/Footer";
 import Product from "../../Components/Product/Product";
 import Recent from "../../Components/Recent/Recent";
 import Delivery from './../../Components/Delivery/Delivery';
@@ -57,6 +58,11 @@ const Home = () => {
             {/* Recent component  */}
             <div className="w-full flex items-center justify-center pb-[80px] pt-[80px]">
                 <Recent></Recent>
+            </div>
+
+            {/* footer component */}
+            <div className="w-full flex items-center justify-center ">
+                <Footer></Footer>
             </div>
 
         </div>
